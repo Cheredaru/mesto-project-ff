987d6d5fb3ecc1f2d8a0824cf2072994289c79ba
Cheredaru <pigrev339@yandex.ru>
2024-08-22 16:05:52 +0300

    Commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,23 @@
-// @todo: Темплейт карточки
+const placesList = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+initialCards.forEach((el) => {
+  placesList.append(createCard(el.name, el.link, deleteCard));
+})
 
-// @todo: Функция создания карточки
+function createCard(name, link, deleteCard) {
+  const placesItem = cardTemplate.querySelector('.places__item').cloneNode(true);
+  const deleteButton = placesItem.querySelector('.card__delete-button');
 
-// @todo: Функция удаления карточки
+  placesItem.querySelector('.card__image').src = link;
+  placesItem.querySelector('.card__title').textContent = name;
 
-// @todo: Вывести карточки на страницу
+  deleteButton.addEventListener('click', deleteCard);
+  return placesItem;
+}
+
+function deleteCard(evt) {
+  const eventTarget = evt.target;
+  const placesItem = eventTarget.closest('.places__item');
+  placesItem.remove();
+}
