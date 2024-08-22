8b18f4602b0656c8641134123fef88b492eab563
Cheredaru <pigrev339@yandex.ru>
2024-08-22 19:03:57 +0300

    Commit v2.0

--- scripts/cards.js
+++ scripts/cards.js
@@ -2,25 +2,31 @@ const initialCards = [
     {
       name: "Архыз",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+      alt: "Горы Карачаево-Черкесии",
     },
     {
       name: "Челябинская область",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+      alt: "Озеро Челябинской области",
     },
     {
       name: "Иваново",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+      alt: "Панельные дома в Иваново",
     },
     {
       name: "Камчатка",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+      alt: "Природа Камчатки",
     },
     {
       name: "Холмогорский район",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+      alt: "Железная дорога",
     },
     {
       name: "Байкал",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+      alt: "Озеро Байкал",
     }
 ];
--- scripts/index.js
+++ scripts/index.js
@@ -2,14 +2,15 @@ const placesList = document.querySelector('.places__list');
 const cardTemplate = document.querySelector('#card-template').content;
 
 initialCards.forEach((el) => {
-  placesList.append(createCard(el.name, el.link, deleteCard));
+  placesList.append(createCard(el.name, el.link, el.alt, deleteCard));
 })
 
-function createCard(name, link, deleteCard) {
+function createCard(name, link, alt, deleteCard) {
   const placesItem = cardTemplate.querySelector('.places__item').cloneNode(true);
   const deleteButton = placesItem.querySelector('.card__delete-button');
 
   placesItem.querySelector('.card__image').src = link;
+  placesItem.querySelector('.card__image').alt = alt;
   placesItem.querySelector('.card__title').textContent = name;
 
   deleteButton.addEventListener('click', deleteCard);
@@ -17,7 +18,6 @@ function createCard(name, link, deleteCard) {
 }
 
 function deleteCard(evt) {
-  const eventTarget = evt.target;
-  const placesItem = eventTarget.closest('.places__item');
+  const placesItem = evt.target.closest('.places__item');
   placesItem.remove();
 }
